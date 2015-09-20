0951edd0897e84a21a4c79e81f86fcd8737880ec
sffish <sunfei@ezjie.com>
2015-09-20 11:12:52 +0800

    ss

---
+++ rrr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rrr
+//
+//  Created by sun on 15/9/20.
+//  Copyright © 2015年 sunfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rrr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rrr
+//
+//  Created by sun on 15/9/20.
+//  Copyright © 2015年 sunfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
